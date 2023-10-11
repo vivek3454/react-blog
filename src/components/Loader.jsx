5cec285ba4c06843afedcbfcb3e29b2264ba3873
Vivek <vivekparde2411@gmail.com>
2023-10-11 19:49:57 +0530

    Fixed loader height issue

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import ReactLoading from "react-loading";
 
 const Loader = ({ height = "80vh" }) => {
   return (
-    <div className={`w-full h-[${height}] flex justify-center items-center bg-white dark:bg-black`}>
+    <div className={`w-full min-h-[${height}] flex justify-center items-center bg-white dark:bg-black`}>
       <ReactLoading type={"bars"} color={"#6366F1"} height={"5%"} width={"5%"} />
     </div>
   );
